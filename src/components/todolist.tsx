2a5aae548abdd0cd86f1e4cc15248b1d872c5f0c
brickardo <neobzorsheek@gmail.com>
2024-12-14 08:41:35 +0100

    New card adding realized

--- src/components/todolist.tsx
+++ src/components/todolist.tsx
@@ -1,7 +1,30 @@
-function ToDoList(){
-    return (
-        <div>
+import { useState } from "react";
+import {ToDoListType} from "../types/ToDoListType";
 
+function ToDoList(){
+    const [counter, setCounter] = useState(0);
+    const [cards, setCards] = useState<ToDoListType>([]);
+
+    const ToDoCardCreate = () =>{
+        setCounter(counter+1);
+        setCards([...cards, {header: `name is ${counter}`, text: 'Beleberda', creatingDate: "Hz"}])
+    }
+
+    return (
+        <div id="ToDo_wapper">
+            <button onClick={ToDoCardCreate}>
+                +
+            </button>
+            <div>
+                <ul>
+                    {cards.map((card, index)=>(
+                        <li className="ToDoCard" key={index}>
+                            <h3>{card.header}</h3>
+                            <p>{card.text}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
