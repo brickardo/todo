a2366af27ce612c20bfde231211119a7af70dcbc
brickardo <neobzorsheek@gmail.com>
2024-12-14 08:57:53 +0100

    card deleting realize

--- src/components/todolist.tsx
+++ src/components/todolist.tsx
@@ -2,14 +2,17 @@ import { useState } from "react";
 import {ToDoListType} from "../types/ToDoListType";
 
 function ToDoList(){
-    const [counter, setCounter] = useState(0);
+    const [counter, setCounter] = useState(1);
     const [cards, setCards] = useState<ToDoListType>([]);
 
     const ToDoCardCreate = () =>{
+        setCards([...cards, {id: counter, header: `name is ${counter}`, text: 'Beleberda', creatingDate: "Hz"}])
         setCounter(counter+1);
-        setCards([...cards, {header: `name is ${counter}`, text: 'Beleberda', creatingDate: "Hz"}])
     }
 
+    const DeleteCard = (id: number) =>{
+        setCards((previusCards)=> previusCards.filter((item)=> item.id !== id));
+    }
     return (
         <div id="ToDo_wapper">
             <button onClick={ToDoCardCreate}>
@@ -17,10 +20,11 @@ function ToDoList(){
             </button>
             <div>
                 <ul>
-                    {cards.map((card, index)=>(
-                        <li className="ToDoCard" key={index}>
+                    {cards.map((card)=>(
+                        <li className="ToDoCard" key={card.id}>
                             <h3>{card.header}</h3>
                             <p>{card.text}</p>
+                            <button onClick={()=>DeleteCard(card.id)}>Delete me</button>
                         </li>
                     ))}
                 </ul>
