865d1458a14af9f44d2e5fff73fa0a70dc42d4e2
brickardo <neobzorsheek@gmail.com>
2024-12-15 16:08:24 +0100

    component take paramaters fix and text input add

--- src/components/todoCard.tsx
+++ src/components/todoCard.tsx
@@ -1,27 +1,37 @@
 import React, { useState } from "react"
 
-const TodoCard = React.memo((id: number, DeleteCard) => {
+interface TodoCardProps{
+    id: number;
+    DeleteCard: (id:number)=>void;
+}
+
+export const TodoCard: React.FC<TodoCardProps> = React.memo(({id, DeleteCard}: TodoCardProps) => {
     const [header, setHeader] = useState<string>(`name is ${id}`);
     const [text, setText] = useState<string>("");
 
     const handleHeaderChangeKeyPress = (e: React.KeyboardEvent<HTMLInputElement>, setFunc: any) => {
-        if (e.key === 'Enter'){
+        if (e.key === 'Enter') {
             setFunc(e.currentTarget.value);
         }
     }
 
     return (
         <li className="ToDoCard" key={id}>
+
             <div className="CardHeaderSection_wrapper">
-                <input 
+                <input
                     type="text" defaultValue={header}
                     onKeyDown={(e) => handleHeaderChangeKeyPress(e, setHeader)}
                 />
                 <h3 className="CardHeader">{header}</h3>
             </div>
-            
-            <div className="CardTextSection_wrapper">
 
+
+            <div className="CardTextSection_wrapper">
+                <input
+                    type="text" defaultValue={text}
+                    onKeyDown={(e) => handleHeaderChangeKeyPress(e, setText)}
+                />
                 <p className="CardText">{text}</p>
             </div>
             <button className="cardDeteleButton" onClick={() => DeleteCard(id)}>Delete me</button>
